d9b2556b3c90c2c0505121eb83076628108e28d2
sivakrishnateja-tammisetty <sivakrishnateja07@gmail.com>
2023-04-07 10:32:21 +0530

    BUBBLESORT ADDED

---
+++ b_sort.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+void swap(int *x,int* y)
+{
+	int temp;
+	temp=*x;
+	*x=*y;
+	*y=temp;
+}
+void b_sort(int *a,int n)
+{
+	int i,j;
+	for(i=0;i<n-1;i++)
+	{
+		for(j=0;j<n-1-i;j++)
+		{
+			if(a[j]>a[j+1])
+			{
+				swap(&a[j],&a[j+1]);
+			}
+		}
+	}
+}
+void display(int *a,int n)
+{
+	int i;
+	for(i=0;i<n;i++)
+	{
+		printf("%d\t",a[i]);
+	}
+}
+int main()
+{
+	int arr[]={11,2,12,1,4,5};
+	int SIZE=sizeof(arr)/sizeof(arr[0]);
+	b_sort(arr,SIZE);
+	display(arr,SIZE);
+	return 0;
+}
