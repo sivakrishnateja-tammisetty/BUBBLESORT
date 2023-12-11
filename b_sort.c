d4b4be975639b4050c6ca73dd8f617313132bfa7
Sivakrishnateja Tammisetty <98377305+sivakrishnateja-tammisetty@users.noreply.github.com>
2023-12-11 13:15:32 +0530

    Update b_sort.c

--- b_sort.c
+++ b_sort.c
@@ -1,6 +1,52 @@
-BUBBLE SORT:
+BUBBLE SORT: pushes the maximum elemnt at last by adjacent swaps.
+	
  * no.of pass required = no.of elements -1
  * No.of loops required = 2( 1 for passes and another for comparsions)
+
+
+
+
+Eg:
+	#include <bits/stdc++.h>
+using namespace std;
+void bubble_sort(int *a,int n)
+{
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-1-i;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                int temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+}
+void display(int *a,int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+}
+int main()
+{
+ int a[]={11,2,4,2,23,20};
+ int n=sizeof(a)/sizeof(a[0]);
+ bubble_sort(a,n);
+ display(a, n);
+ 
+    return 0;
+}
+
+o/p: 2 2 4 11 20 23
+
+
+
+
+	
 
 #include<stdio.h>
 void swap(int *x,int* y)
